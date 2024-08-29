a4f2bb1333c6b3c8e1d12f73004adb6f654f5314
erfanghorbanee <agh.business@proton.me>
2024-08-29 10:19:23 +0200

    refactor: Update homepage URL in navbar brand link

--- Django-Shop/config/urls.py
+++ Django-Shop/config/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('home.urls')),
+    path('', include('home.urls'), name='home'),
 
     # allauth
     path('accounts/', include('allauth.urls')),
