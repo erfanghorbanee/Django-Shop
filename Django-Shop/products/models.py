5720740e3fdbc5907f5699529e519f5f55b8fd46
erfanghorbanee <agh.business@proton.me>
2025-05-07 17:00:38 +0200

    refactor: Update Product model to use dynamic image upload path and separate ProductImage model

--- Django-Shop/products/models.py
+++ Django-Shop/products/models.py
@@ -3,6 +3,7 @@ from django.utils.text import slugify
 from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
+import os
 
 # Custom validator for image size
 def validate_image_size(image):
@@ -11,6 +12,9 @@ def validate_image_size(image):
     if file_size > limit_mb * 1024 * 1024:
         raise ValidationError(f"Max size of file is {limit_mb} MB")
 
+# Function to determine the upload path for product images
+def product_image_upload_path(instance, filename):
+    return os.path.join('products', instance.product.name, filename)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -32,7 +36,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     stock = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
-    image = models.ImageField(upload_to='products/', validators=[FileExtensionValidator(["jpg", "jpeg", "png"]), validate_image_size])
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -47,3 +50,12 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to=product_image_upload_path, 
+                              validators=[FileExtensionValidator(["jpg", "jpeg", "png"]), validate_image_size])
+
+    def __str__(self):
+        return f"{self.product.name} Image"
