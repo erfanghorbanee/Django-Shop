9e2a4ca989d7d0ed138db708694525a5ba0b4e51
erfanghorbanee <agh.business@proton.me>
2025-06-05 12:29:53 +0200

    feat: Enhance career application form with phone number field and PDF validation
    - improve style as well

--- Django-Shop/careers/forms.py
+++ Django-Shop/careers/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import CareerApplication
+from phonenumber_field.formfields import SplitPhoneNumberField
 
 class CareerApplicationForm(forms.ModelForm):
+    phone = SplitPhoneNumberField(required=False)
     class Meta:
         model = CareerApplication
         fields = ['name', 'email', 'phone', 'resume', 'cover_letter'] 
--- Django-Shop/careers/models.py
+++ Django-Shop/careers/models.py
@@ -1,12 +1,49 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+import magic
 
-# Create your models here.
+
+def validate_pdf(file):
+    """
+    This performs two checks:
+    1. Ensures the file size is within the allowed maximum (in bytes).
+    2. Uses libmagic (via python-magic) to analyze the file's magic bytes and verify its actual MIME.
+    """
+
+    # Maximum allowed size (5 MB)
+    max_size = 5 * 1024 * 1024
+
+    # Check file size
+    if file.size > max_size:
+        raise ValidationError("File size must be 5 MB or less.")
+
+    # Read the first 2048 bytes (recommendation from python-magic)
+    sample = file.read(2048)
+
+    # Reset the file pointer to the beginning, so Django can process the file later
+    file.seek(0)
+
+    # Detect file's actual MIME type based on content
+    mime = magic.from_buffer(sample, mime=True)
+
+    # Check if the detected MIME type is PDF
+    if mime != 'application/pdf':
+        raise ValidationError("Uploaded file is not a valid PDF.")
+
 
 class CareerApplication(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = models.CharField(max_length=20, blank=True)
-    resume = models.FileField(upload_to='resumes/')
+    phone = PhoneNumberField(blank=True)
+    resume = models.FileField(
+        upload_to='resumes/',
+        validators=[
+            FileExtensionValidator(allowed_extensions=['pdf']),
+            validate_pdf
+        ]
+    )
     cover_letter = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
