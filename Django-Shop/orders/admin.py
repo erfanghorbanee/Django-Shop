8761f3a2f68cf28eaaf549daafa48ea18467b411
erfanghorbanee <agh.business@proton.me>
2025-08-08 19:36:34 +0200

    Improved orders app and bug fixes
    - users/admin: registered Address and Wishlist.
    - removed duplicate messages block; standardized title block; fixed product image url
    - deleted unused templates

--- Django-Shop/orders/admin.py
+++ Django-Shop/orders/admin.py
@@ -1,19 +1,43 @@
 from django.contrib import admin
+
 from .models import Order, OrderItem, PaymentMethod
+
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
 
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('order_number', 'user', 'status', 'total_amount', 'created_at')
-    list_filter = ('status', 'created_at')
-    search_fields = ('order_number', 'user__email')
+    list_display = (
+        "order_number",
+        "user",
+        "status",
+        "shipping_address",
+        "billing_address",
+        "total_amount",
+        "created_at",
+    )
+    list_filter = ("status", "created_at")
+    search_fields = (
+        "order_number",
+        "user__email",
+        "shipping_address__street_address",
+        "billing_address__street_address",
+    )
+    raw_id_fields = ("user", "shipping_address", "billing_address", "payment_method")
     inlines = [OrderItemInline]
+
 
 @admin.register(PaymentMethod)
 class PaymentMethodAdmin(admin.ModelAdmin):
-    list_display = ('user', 'payment_type', 'card_holder_name', 'is_default', 'created_at')
-    list_filter = ('payment_type', 'is_default', 'created_at')
-    search_fields = ('user__email', 'card_holder_name')
+    list_display = (
+        "user",
+        "payment_type",
+        "card_holder_name",
+        "is_default",
+        "created_at",
+    )
+    list_filter = ("payment_type", "is_default", "created_at")
+    search_fields = ("user__email", "card_holder_name")
--- Django-Shop/users/admin.py
+++ Django-Shop/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import Address, CustomUser, Wishlist
 
 admin.site.register(CustomUser)
-
+admin.site.register(Address)
+admin.site.register(Wishlist)
