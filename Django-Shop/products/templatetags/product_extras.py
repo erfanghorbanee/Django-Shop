fa6875a286fe4a7e5fefdd76964b31dd11594e4a
erfanghorbanee <agh.business@proton.me>
2025-08-22 17:43:06 +0200

    feat(home): Add carousel for special discounts and most bought products sections

--- Django-Shop/products/templatetags/product_extras.py
+++ Django-Shop/products/templatetags/product_extras.py
@@ -2,10 +2,18 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def sub(value, arg):
     """Subtract the arg from the value"""
     try:
         return float(value) - float(arg)
     except (ValueError, TypeError):
-        return value 
+        return value
+
+
+@register.filter
+def chunked(value, n):
+    """Break a list into chunks of size n."""
+    n = int(n)
+    return [value[i : i + n] for i in range(0, len(value), n)]
